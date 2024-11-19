f72d8ad6957a5af77f594ddc86a3b9d60242f6a5
Zokalyx <zokalyx@gmail.com>
2024-11-19 15:54:23 -0300

    Tests (ver si se parsean las cartas bien individualmente)

--- src/main/test/LectorJsonTest.java
+++ src/main/test/LectorJsonTest.java
@@ -1,6 +1,9 @@
 import edu.fiuba.algo3.modelo.LectorJson;
 import edu.fiuba.algo3.modelo.Poker;
+import edu.fiuba.algo3.modelo.Puntaje;
 import edu.fiuba.algo3.modelo.comodin.Comodin;
+import edu.fiuba.algo3.modelo.jugada.JugadaNula;
+import edu.fiuba.algo3.modelo.palo.Trebol;
 import edu.fiuba.algo3.modelo.tarot.Tarot;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,5 +33,49 @@ public class LectorJsonTest {
         ArrayList<Tarot> cartas = lectorJson.leerTarots();
 
         assertEquals(15, cartas.size());
+    }
+
+    @Test
+    public void test04LosPokersSeLeenCorrectamente() {
+        LectorJson lectorJson = new LectorJson();
+        ArrayList<Poker> cartas = lectorJson.leerMazo();
+
+        Poker carta = cartas.get(0);
+        Poker cartaComparacion = new Poker("As", new Trebol(), 10, 1);
+        Puntaje puntaje = new Puntaje(0, 0);
+        carta.modificarPuntaje(puntaje);
+
+        assertTrue(carta.esMismoPaloQue(cartaComparacion));
+        assertTrue(carta.esMismoSimboloQue(cartaComparacion));
+        assertEquals(10, puntaje.calcularTotal());
+    }
+
+    @Test
+    public void test05LosComodinesSeLeenCorrectamente() {
+        LectorJson lectorJson = new LectorJson();
+        ArrayList<Comodin> cartas = lectorJson.leerComodines();
+
+        Comodin carta = cartas.get(0);  // Comodín descarte 30 x 1
+        Puntaje puntaje = new Puntaje(0, 0);
+        carta.modificarPuntaje(puntaje, new JugadaNula(new ArrayList<>()), 2);  // 2 descartes
+
+        assertEquals(120, puntaje.calcularTotal());
+    }
+
+    @Test
+    public void test06LosTarotSeLeenCorrectamente() {
+        LectorJson lectorJson = new LectorJson();
+        ArrayList<Tarot> cartas = lectorJson.leerTarots();
+
+        Tarot tarot = cartas.get(0);  // Tarot sobre carta: agrega 10 x 2
+        Poker poker = new Poker("As", new Trebol(), 10, 1);
+        ArrayList<Poker> pokers = new ArrayList<>();
+        pokers.add(poker);
+
+        tarot.modificar(pokers);
+
+        Puntaje puntaje = new Puntaje(0, 0);
+        poker.modificarPuntaje(puntaje);
+        assertEquals(20, puntaje.calcularTotal());
     }
 }
