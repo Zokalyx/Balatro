aba7ee73253d2f4bde6f36ffe65d759af78d2252
Zokalyx <zokalyx@gmail.com>
2024-11-24 22:51:25 -0300

    Menús básicos de JavaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,5 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
     requires json.simple;
-    exports edu.fiuba.algo3;
-    exports edu.fiuba.algo3.modelo;
-    exports edu.fiuba.algo3.modelo.jugada;
-    exports edu.fiuba.algo3.modelo.palo;
-    exports edu.fiuba.algo3.modelo.tarot;
-    exports edu.fiuba.algo3.modelo.comodin;
-    exports edu.fiuba.algo3.modelo.contenedores;
+    exports edu.fiuba.algo3.vistas;
 }
