0e8487efe69057fc490dabe128e298d0564dd363
Zokalyx <zokalyx@gmail.com>
2024-12-02 00:04:16 -0300

    Imágenes de palos en las cartas

--- src/main/java/edu/fiuba/algo3/vistas/PokerVista.java
+++ src/main/java/edu/fiuba/algo3/vistas/PokerVista.java
@@ -4,6 +4,8 @@ import edu.fiuba.algo3.modelo.Poker;
 import edu.fiuba.algo3.modelo.palo.*;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
 import java.util.Observable;
@@ -12,32 +14,52 @@ import java.util.Observer;
 public class PokerVista extends CartaVista implements Observer {
     public PokerVista(Poker poker) {
         String simbolo = simboloVisible(poker.getSimbolo());
-        String palo = paloVisible(poker.getPalo());
-        String color = colorPalo(poker.getPalo());
+        Palo palo = poker.getPalo();
 
-        Pane conjuntoSuperior = crearConjuntoSimboloPalo(simbolo, palo, color);
-        Pane conjuntoInferior = crearConjuntoSimboloPalo(simbolo, palo, color);
+        Pane conjuntoSuperior = crearConjuntoSimboloPalo(simbolo, palo);
+        Pane conjuntoInferior = crearConjuntoSimboloPalo(simbolo, palo);
         conjuntoInferior.setRotate(180);
+
+        StackPane.setAlignment(conjuntoSuperior, Pos.TOP_LEFT);
+        StackPane.setAlignment(conjuntoInferior, Pos.BOTTOM_RIGHT);
 
         getChildren().addAll(conjuntoSuperior, conjuntoInferior);
     }
 
-    private String colorPalo(Palo palo) {
-        if (palo.equals(new Pica()) || palo.equals(new Trebol())) {
-            return "black";
+    private ImageView paloVisible(Palo palo) {
+        Image image;
+        if (palo.equals(new Diamante())) {
+            image = new Image(getClass().getResource("/diamante.png").toExternalForm());
+        } else if (palo.equals(new Trebol())) {
+            image = new Image(getClass().getResource("/trebol.png").toExternalForm());
+        } else if (palo.equals(new Corazon())) {
+            image = new Image(getClass().getResource("/corazon.png").toExternalForm());
+        } else if (palo.equals(new Pica())) {
+            image = new Image(getClass().getResource("/pica.png").toExternalForm());
         } else {
+            throw new RuntimeException("Palo inexistente");
+        }
+        return new ImageView(image);
+    }
+
+    private String colorPalo(Palo palo) {
+        if (palo.equals(new Diamante()) || palo.equals(new Corazon())) {
             return "red";
+        } else {
+            return "black";
         }
     }
 
-    private Pane crearConjuntoSimboloPalo(String simbolo, String palo, String color) {
+    private Pane crearConjuntoSimboloPalo(String simbolo, Palo palo) {
+        String color = colorPalo(palo);
         Label nombreLabel = new Label(simbolo);
-        Label paloLabel = new Label(palo);
-
         nombreLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: " + color + "; -fx-font-size: 14;");
-        paloLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: " + color + "; -fx-font-size: 14;");
 
-        return new VBox(nombreLabel, paloLabel);
+        VBox vbox = new VBox(nombreLabel, paloVisible(palo));
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setMaxHeight(40);
+        vbox.setMaxWidth(20);
+        return vbox;
     }
 
     private String simboloVisible(String simbolo) {
@@ -55,22 +77,7 @@ public class PokerVista extends CartaVista implements Observer {
         }
     }
 
-    private String paloVisible(Palo palo) {
-        if (palo.equals(new Diamante())) {
-            return "♢";
-        } else if (palo.equals(new Trebol())) {
-            return "♧";
-        } else if (palo.equals(new Corazon())) {
-            return "♡";
-        } else if (palo.equals(new Pica())) {
-            return "♤";
-        } else {
-            throw new RuntimeException("Palo inexistente");
-        }
-    }
-
     @Override
     public void update(Observable o, Object arg) {
-
     }
 }
