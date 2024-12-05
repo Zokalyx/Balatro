d29e80dde6d54df9e849ceef33c15e218b05d63a
Zokalyx <zokalyx@gmail.com>
2024-12-05 15:32:37 -0300

    Fix bug de transición de número

--- src/main/java/edu/fiuba/algo3/vistas/TransicionNumero.java
+++ src/main/java/edu/fiuba/algo3/vistas/TransicionNumero.java
@@ -7,30 +7,32 @@ import javafx.util.Duration;
 class TransicionNumero extends Transition {
     double valorInicial;
     double valorFinal;
+    double valorActual;
     Label label;
     boolean redondear;
 
     public TransicionNumero(Label label, double valorInicial, boolean redondear) {
         this.label = label;
         this.valorInicial = valorInicial;
+        this.valorActual = valorInicial;
         setCycleDuration(Duration.seconds(0.5));
         setCycleCount(1);
         this.redondear = redondear;
     }
 
     public void setValorFinal(int valorFinal) {
-        this.valorInicial = this.valorFinal;
+        this.valorInicial = valorActual;
         this.valorFinal = valorFinal;
     }
 
     public void setValorFinal(double valorFinal) {
-        this.valorInicial = this.valorFinal;
+        this.valorInicial = valorActual;
         this.valorFinal = valorFinal;
     }
 
     @Override
     protected void interpolate(double frac) {
-        double valorActual = valorInicial + frac * (valorFinal - valorInicial);
+        valorActual = valorInicial + frac * (valorFinal - valorInicial);
         if (redondear) {
             label.setText(String.valueOf(Math.round(valorActual)));
         } else {
